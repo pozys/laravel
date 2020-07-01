4231c428dd0a51640845fd55af750f5508558c1f
pozys <pozys@mail.ru>
2020-07-02 01:59:27 +0300

    HomeWork 3

---
+++ resources/views/news/news_category.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+@extends('menu')
+@extends('footer')
+@extends('header')
+
+@section('content')
+@foreach ($news as $key => $item) 
+<h1 class="display-4 font-italic">{{ $item['title'] }} </h1>
+<p class="lead mb-0"><a href=" {{ route('news.getNewsById', $key) }} " class="text-white font-weight-bold">Читать дальше..</a></p>
+</a>
+@endforeach
+@endsection
